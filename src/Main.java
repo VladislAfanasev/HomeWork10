e462d5eae8a678c188a9f5f7c6758695f5f8db36
VladislAfanasev <vladucha.afanasjev@mail.ru>
2023-10-14 18:42:42 +1000

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+// Задача №1. Вывести в консоль имя сотрудника
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName+" " + firstName+" " + middleName;
+        System.out.println("Ф. И. О. сотрудника — "+ fullName);
+        System.out.println();
+
+// Задача №2. Вывести в консоль имя сотрудника заглавными буквами
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — "+fullName.toUpperCase());
+        System.out.println();
+
+// Задача №3. Написать программу, которая выводит имя сотрудника заменяя символ "ё" на "е"
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — "+fullName.replace('ё', 'е'));
+
     }
 }
